9d9c7bc5d0df778cea2e426342f240bef304857a
agrimony <78861652+agriimony@users.noreply.github.com>
2025-11-23 09:59:00 +0800

    Update keyphrase

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,4 +1,4 @@
 // Keyphrase for valid HIGHER casts
-export const KEYPHRASE_REGEX = /cooking\s+up\s+a\s+higher\s+steak\s*(.+)/i;
+export const KEYPHRASE_REGEX = /cooking\s+up\s+a\s+higher\s+steak!\s*(.+)/i;
 export const KEYPHRASE_TEXT = "Cooking up a higher steak!";
 
